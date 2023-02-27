6e9efca3827593ef243fc09dff893b3d8c877b6f
daisuke.miyazoe <daisuke.miyazoe@torico-tokyo.com>
2023-02-27 23:30:21 +0900

    追加

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,10 @@
 from django.views import generic
+from .forms import Inquiryform
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = Inquiryform
