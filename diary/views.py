8d70f78d7ea5da078f076cb85d77ed406c547d68
宮副大佑 <miyazoedaisuke@miyafukudaisukenoMacBook-Pro.local>
2023-03-13 08:24:59 +0900

    ツイカ

--- diary/views.py
+++ diary/views.py
@@ -3,6 +3,7 @@ import logging
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import Inquiryform
+from django.contrib import messages
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,7 @@ class InquiryView(generic.FormView):
 
     def form_valid(self, form):
         form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
 
